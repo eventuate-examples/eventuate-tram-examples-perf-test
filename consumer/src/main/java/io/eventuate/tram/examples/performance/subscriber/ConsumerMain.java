640e829d8add58bb0c2be4c3469176784e9ab9d9
Chris Richardson <chris@chrisrichardson.net>
2022-09-21 13:49:47 -0700

    Increased Hikari pool size + misc changes

--- consumer/src/main/java/io/eventuate/tram/examples/performance/subscriber/ConsumerMain.java
+++ consumer/src/main/java/io/eventuate/tram/examples/performance/subscriber/ConsumerMain.java
@@ -7,23 +7,27 @@ import io.eventuate.tram.examples.performance.common.PerformanceTestEvent;
 import io.eventuate.tram.examples.performance.common.TestAggregate;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 @Import({EventuateCommonJdbcOperationsConfiguration.class}) // Not right
 public class ConsumerMain {
 
+    private static Logger logger = LoggerFactory.getLogger(ConsumerMain.class);
 
     static class TramEventTestEventConsumer {
 
-        private final MeterRegistry meterRegistry;
         private final Counter counter;
 
         public TramEventTestEventConsumer(MeterRegistry meterRegistry) {
-            this.meterRegistry = meterRegistry;
             this.counter = meterRegistry.counter("consumer.events.consumed");
         }
 
@@ -36,7 +40,14 @@ public class ConsumerMain {
 
         public void handlePerformanceTestEvent(DomainEventEnvelope<PerformanceTestEvent> event) {
             // System.out.println("Received: " + event);
+            logger.info("start");
             counter.increment();
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            logger.info("end");
         }
     }
 
@@ -46,10 +57,11 @@ public class ConsumerMain {
         return domainEventDispatcherFactory.make("eventDispatcher", target.domainEventHandlers());
     }
 
-    @Bean
-    public TopicPartitionToSwimlaneMapping topicPartitionToSwimlaneMapping() {
-        return (topicPartition, messageKey) -> messageKey.hashCode() % 200;
-    }
+//    @Bean
+//    public TopicPartitionToSwimlaneMapping topicPartitionToSwimlaneMapping() {
+//        // Must match spring.datasource.hikari.maximumPoolSize=50
+//        return (topicPartition, messageKey) -> Math.abs(Objects.hash(topicPartition, messageKey)) % 50;
+//    }
 
     @Bean
     public TramEventTestEventConsumer tramEventTestTarget(MeterRegistry meterRegistry) {
